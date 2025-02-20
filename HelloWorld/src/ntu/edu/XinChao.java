9c9e8a9b81af86f82e94fbbb1519598d5f301cd5
NgHxon <son.nh.64cntt@ntu.edu.vn>
2025-02-20 07:31:55 +0700

    ví dụ
    lab1

--- HelloWorld/src/ntu/edu/XinChao.java
+++ HelloWorld/src/ntu/edu/XinChao.java
@@ -13,7 +13,7 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Tôi lần đầu học java, và đang thử nghiệm quản lý mã nguồn với GitHub");
+		System.out.println("Tôi lần đầu học java, và đang thử nghiệm quản lý mã nguồn với GitHub");	
 
 	}
 
